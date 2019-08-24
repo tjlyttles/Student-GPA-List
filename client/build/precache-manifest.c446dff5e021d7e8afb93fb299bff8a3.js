6ce4bf7cf85a3288e816abd8c9722cd20a4ce75b
tjlyttles <tjlyttles84@gmail.com>
2019-08-23 17:32:55 -0700

    added github pages info

---
+++ client/build/precache-manifest.c446dff5e021d7e8afb93fb299bff8a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a5f813a5e1676bb81611",
+    "url": "/static/js/main.d5f93d9a.chunk.js"
+  },
+  {
+    "revision": "2576eda87adc89e7ff37",
+    "url": "/static/js/2.a6339723.chunk.js"
+  },
+  {
+    "revision": "2576eda87adc89e7ff37",
+    "url": "/static/css/2.208176a4.chunk.css"
+  },
+  {
+    "revision": "8af6bfb84ef18f1022760a93dd12f6df",
+    "url": "/index.html"
+  }
+];
